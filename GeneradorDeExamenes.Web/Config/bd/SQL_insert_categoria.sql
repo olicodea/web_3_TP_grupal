3413008fede7861dfc657117a5da97458a44104b
Arielandriel30 <ariel.landriel@hotmail.com>
2024-06-22 23:30:02 -0300

    insert Categoria

---
+++ GeneradorDeExamenes.Web/Config/bd/SQL_insert_categoria.sql
@@ -0,0 +1,16 @@
+USE [ExamenIA]
+GO
+SET IDENTITY_INSERT [dbo].[Categoria] ON 
+GO
+INSERT [dbo].[Categoria] ([IdCategoria], [Nombre]) VALUES (1, N'arte')
+GO
+INSERT [dbo].[Categoria] ([IdCategoria], [Nombre]) VALUES (2, N'naturales')
+GO
+INSERT [dbo].[Categoria] ([IdCategoria], [Nombre]) VALUES (3, N'informática')
+GO
+INSERT [dbo].[Categoria] ([IdCategoria], [Nombre]) VALUES (4, N'sociales')
+GO
+INSERT [dbo].[Categoria] ([IdCategoria], [Nombre]) VALUES (5, N'economía')
+GO
+SET IDENTITY_INSERT [dbo].[Categoria] OFF
+GO
